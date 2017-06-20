25b9e12966c531e905c850a90004a009d6f052ec
Sean Dooher <sean.dooher@berkeley.edu>
2017-06-20 13:29:15 -0700

    mend

---
+++ Rehood Naes/Interfaces/IUpdateable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rehood_Naes.Interfaces
+{
+    interface IUpdateable
+    {
+    }
+}
